eee95e2b7972f8b65518ed35ee555783f6707231
Walker Fowlkes <walkerffo22@gmail.com>
2024-04-29 18:51:24 -0700

    remove target folder...

--- target/debug/deps/scala2kt-1f78c021209a1968.d
+++
@@ -1,5 +0,0 @@
-/home/walksanator/git/rust/scala2kt/target/debug/deps/scala2kt-1f78c021209a1968: src/main.rs
-
-/home/walksanator/git/rust/scala2kt/target/debug/deps/scala2kt-1f78c021209a1968.d: src/main.rs
-
-src/main.rs:
--- target/debug/scala2kt.d
+++
@@ -1,1 +0,0 @@
-/home/walksanator/git/rust/scala2kt/target/debug/scala2kt: /home/walksanator/git/rust/scala2kt/src/main.rs
